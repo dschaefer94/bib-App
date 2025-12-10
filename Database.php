2cd24de810a98277de203fc79a549705f6690792
Ramiz Niftaliev <ramiz97.dev@gmail.com>
2025-12-10 13:03:59 +0100

    d
    d

--- Database.php
+++ Database.php
@@ -1,8 +1,16 @@
 
 <?php
 // Datenbank-Daten, damit diese nicht hardgecodet im index.php rumliegt.
-$host = "localhost:3306";
-$dbname = "pbd2h24asc_bibapp";
-$username = "pbd2h24asc_backendboi";
-$password = "T3llMeWhy!";
+$host = "localhost";
+$dbname = "stundenplan_db";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Verbindung zur Datenbank erfolgreich!";
+} catch (PDOException $e) {
+    die("Fehler bei der Verbindung: " . $e->getMessage());
+}
 ?>
