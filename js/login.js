21b9eb6e07601e51f3b1f7dab1f6632d4af9775c
Daniel Schäfer <daniel.schaefer@edu.bib.de>
2026-01-21 22:23:54 +0100

    Frontendimplementierung der Loginfunktion

---
+++ js/login.js
@@ -0,0 +1,45 @@
+document.getElementById("login-btn").addEventListener("click", async (event) => {
+    event.preventDefault();
+    const email = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const resultEl = document.getElementById("result");
+
+    if (!email || !password) {
+        resultEl.textContent = "Введите email и пароль!";
+        resultEl.style.color = "red";
+        return;
+    }
+
+
+    const url = "http://localhost/bibapp_xampp/restapi.php/user/login";
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email: email,   // oder username
+                passwort: password,
+                credentials: "include"
+            })
+
+        });
+
+        const data = await response.json();
+        console.log("Server response:", data);
+
+        if (data.success) {
+            resultEl.textContent = `✔ Glückwunsch!, ${data.user.email}`;
+            resultEl.style.color = "green";
+            window.location.href = "startseite.html";
+
+        } else {
+            resultEl.textContent = `❌ ${data.message}`;
+            resultEl.style.color = "red";
+        }
+    } catch (err) {
+        console.error("Fetch error:", err);
+        resultEl.textContent = `Fehler: ${err.message}`;
+        resultEl.style.color = "red";
+    }
+});
