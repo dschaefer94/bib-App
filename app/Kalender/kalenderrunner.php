7423838414c0d812eabc19120b49a0fa32b70c51
Daniel Schäfer <daniel.schaefer@edu.bib.de>
2026-02-08 01:15:11 +0100

    Sicherheits-Update

--- app/Kalender/kalenderrunner.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Daniel
-//Dieses Skript liegt eigentlich außerhalb des Wegbroots und startet per Cronjob Kalenderupdater.php und logt das Ergebnis
-require_once 'Kalenderupdater.php';
-
-use SDP\Updater;
-
-Updater\updateAlleKalendare();
-
